23927c6dc4610512c75522a5ad5d6cbdaa44480d
Guillaume Hammadi <ghammadi@gmail.com>
2018-02-05 10:49:28 +1100

    WIP simplify the tickets requests code

--- src/javascripts/ticket_sidebar.js
+++ src/javascripts/ticket_sidebar.js
@@ -1,32 +1,53 @@
-import View from 'view';
-import Storage from 'storage'
+import View from 'view'
+// import Storage from 'storage'
 
-const MAX_HEIGHT = 375;
+// import $ from 'jquery';
+// import _ from 'underscore';
 
 class TicketSidebar {
-  constructor(client, data) {
-    this.client = client;
-    this._metadata = data.metadata;
-    this._context = data.context;
 
-    this.storage = new Storage(this._metadata.installationId);
+  constructor(client, data) {
+    this.client = client
+    this._metadata = data.metadata
+    this._context = data.context
+
     this.view = new View({ afterRender: () => {
-      let newHeight = Math.min($('html').height(), MAX_HEIGHT);
-      this.client.invoke('resize', { height: newHeight, width: '100%' });
+      console.log('view loaded')
     }});
 
-    this.getCurrentUser().then(this.renderMain.bind(this));
+    this.getCurrentUser().then((user) => {
+      console.log("user: ", user)
+    })
 
-    this.view.switchTo('loading');
+    this.getTicketRequesterId().then( (requesterId) => {
+      this.client.request(`/api/v2/users/${requesterId}/tickets/requested.json?sort_by=created_at&sort_order=asc`).then( (ticketsData) => {
+
+        console.log("tickets: ",this.filterResults(ticketsData['tickets']))
+      })
+    })
   }
 
   getCurrentUser() {
-    return this.client.request({ url: '/api/v2/users/me.json' });
+    return this.client.request({ url: '/api/v2/users/me.json' })
   }
 
-  renderMain(data) {
-    this.view.switchTo('main', data.user);
+  getTicketRequesterId() {
+    return this.client.get('ticket.requester').then( (response) => {
+      return response['ticket.requester'].id
+    })
   }
+
+  filterResults(tickets) {
+    return tickets.slice(0, 5)
+  }
+
+  // function handleUserResults(data) {
+  //   $('#main').render('lastfive',{
+  //     lastestFiveArr: lastestFive
+  //   });
+  //   // change app size
+  //   client.invoke('resize', { width: '100%', height: '170px'});
+  // }
 }
 
-export default TicketSidebar;
+export default TicketSidebar
